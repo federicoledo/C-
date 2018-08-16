9a2028effb65600cd27091838960bdb43e3da344
federicoledo <federicoledo13@gmail.com>
2018-08-16 07:36:29 -0300

    Add files via upload

---
+++ ABM_Simple/ABM_Simple/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entidades;
+
+namespace ABM_Simple
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadKey();
+            int xReturn;
+            List<Persona> pp = new List<Persona>();
+
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("Que operacion desea realizar?\n1.Crear Persona\n2.Borrar Persona\n3.Mostrar Personas\n\n\nEn caso de querer salir, presione any key");
+                int.TryParse(Console.ReadLine(), out xReturn);
+                xReturn = Program.Response(xReturn, pp);
+                Console.WriteLine("\n\nPresione una tecla para continuar...");
+            }while (xReturn == 1 || xReturn == 2 || xReturn == 3);
+        }
+
+        public static int Response(int key, List<Persona> pp)
+        {
+            int strResult = 0;
+            switch (key)
+            {
+                case 1:
+                    strResult = 1;
+                    Entidades.Persona.createPersona(pp);
+                    break;
+                case 2:
+                    strResult = 2;
+                    Entidades.Persona.deletePersona(pp);
+                    break;
+                case 3:
+                    strResult = 3;
+                    Entidades.Persona.showPersona(pp);
+                    break;
+                default:
+                    Console.WriteLine("Gracias!");
+                    strResult = 0;
+                    break;
+            }
+            return strResult;
+        }
+    }
+}
---
+++ ABM_Simple/ClassLibrary1/Persona.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Persona
+    {
+        public string name;
+        public string surname;
+        public int dni;
+        public int age;
+
+        public Persona()
+        {
+            this.name = "No";
+            this.surname = "Name";
+            this.dni = 5555555;
+            this.age = 0;
+        }
+
+        public Persona(string n, string s, int d, int a)
+        {
+            this.name = n;
+            this.surname = s;
+            this.dni = d;
+            this.age = a;
+        }
+
+        public Persona createPersona(string n, string s, int d, int a)
+        {
+            this.name = n;
+            this.surname = s;
+            this.dni = d;
+            this.age = a;
+            return this;
+        }
+        public static List<Persona> createPersona(List<Persona> pp)
+        {
+            string xName, xSurname;
+            int xAge, xDNI;
+            Persona xPP = new Persona();
+            pp.Add(xPP);
+            Console.Write("Ingrese el nombre: ");
+            xName = Console.ReadLine();
+            Console.Write("Ingrese el apellido: ");
+            xSurname = Console.ReadLine();
+            Console.Write("Ingrese la edad: ");
+            int.TryParse(Console.ReadLine(), out xAge);
+            Console.Write("Ingrese el DNI: ");
+            int.TryParse(Console.ReadLine(), out xDNI);
+            xPP.createPersona(xName, xSurname, xAge, xDNI);
+            return pp;
+        }
+
+        public static List<Persona> deletePersona(List<Persona> pp)
+        {
+            int xvalue;
+            int xCont = 0;
+            Console.WriteLine("Ingrese el numero del que desea eliminar");
+            foreach (Persona xPersona in pp)
+            {
+                Console.Write("{0}:", xCont + 1);
+                Console.WriteLine("{0}, {1}", pp[xCont].surname, pp[xCont].name);
+                xCont++;
+            }
+            int.TryParse(Console.ReadLine(), out xvalue);
+            pp.Remove(pp[xvalue - 1]);
+            return pp;
+        }
+
+        public static List<Persona> showPersona(List<Persona> pp)
+        {
+            int xCont = 0;
+            foreach (Persona xPersona in pp)
+            {
+                Console.WriteLine("{0}:", xCont + 1);
+                Console.WriteLine("Name: {0}", pp[xCont].name);
+                Console.WriteLine("Surname: {0}", pp[xCont].surname);
+                Console.WriteLine("Age: {0}", pp[xCont].age);
+                Console.WriteLine("DNI: {0}", pp[xCont].dni);
+                Console.WriteLine("-------------------------------------------------------");
+                xCont++;
+            }
+            return pp;
+        }
+    }
+}
